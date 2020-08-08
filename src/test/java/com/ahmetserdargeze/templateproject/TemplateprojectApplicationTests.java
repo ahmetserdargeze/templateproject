adebc6bce135dc363f28fd24da1eddf08e1a38c8
ahmetserdargeze <gezeahmetserdar@gmail.com>
2020-08-08 16:29:03 +0300

    Initial commit

---
+++ src/test/java/com/ahmetserdargeze/templateproject/TemplateprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmetserdargeze.templateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
